4334d1a207a944499ffd39c54fde22ce0281f291
Idan Ofek <idanofek@gmail.com>
2020-03-22 13:00:57 +0200

    Trying again

--- disable-interrupts/source.c
+++ disable-interrupts/source.c
@@ -3,6 +3,7 @@
 #include <linux/kernel.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
+#include <linux/delay.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("SpiderPig");
@@ -17,6 +18,8 @@ static int __init mod_init(void)
 
 	unsigned long flags;
 	local_irq_save(flags);
+	msleep(3000);
+	local_irq_restore(flags);
 
 	return 0;
 }
